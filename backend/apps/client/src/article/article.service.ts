0f1990ac2713ef99c0affc999427399cb9c11fc6
jimin9038 <jimin9038@g.skku.edu>
2024-01-14 03:41:46 +0000

    refactor backend structure

--- backend/apps/client/src/article/article.service.ts
+++ backend/apps/client/src/article/article.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common'
-import { PrismaService } from './prisma.service'
+import { PrismaService } from '@libs/prisma'
 import { Article, Prisma } from '@prisma/client'
 
 @Injectable()
