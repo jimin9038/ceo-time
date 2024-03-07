3130f2c658df6fa94e28ab860b99dccbd47657c5
Jimin Ha <jimin9038@g.skku.edu>
2024-03-07 11:31:27 +0900

    formatting

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,67 +1,71 @@
-import Header from '@/components/ui/Header'
-import { Footer } from '@/components/ui/Footer'
 import BigArticle from '@/components/ui/BigArticle'
+import BigBanner from '@/components/ui/BigBanner'
+import { Footer } from '@/components/ui/Footer'
+import Header from '@/components/ui/Header'
 import MiddleArticle from '@/components/ui/MiddleArticle'
 import SmallArticle from '@/components/ui/SmallArticle'
 import SmallBanner from '@/components/ui/SmallBanner'
-import BigBanner from '@/components/ui/BigBanner'
 
 export default function Home() {
+  const title =
+    '“차세대 수출동력 강화 위해 지속적으로 현장의견에 귀 기울일 것” '
+  const content =
+    '윤진식 한국무역협회(KITA) 회장은 6일 경기도 판교 테크노밸리에 위치한 반도체 테스트 장비 전문 기업 엑시콘(대표 최명배)을 방문하며 취임 후 첫 무역업계 현장소통 행보에 나섰다.'
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-3 ">
       <Header></Header>
-      <section className="flex flex-col w-full lg:flex-row max-w-6xl border-b-2 border-r-gray-500">
-        <div className="w-full lg:w-2/3 h-30 mt-6 lg:pr-6 border-r-2 border-r-gray-100">
-          <BigArticle title="[단독] 신명품 라인업 강화한 신세계인터내셔날, ‘더 로우’ 독점 유통" content="[이코노미스트 이혜리 기자] 패션전문기업 신세계인터내셔날이 ‘MZ세대의 에르메스’로 불리는 ‘더 로우’(The Row)를 새로 론칭하며 신명품 브랜드 포트폴리오 다각화에 나선다. "></BigArticle>
+      <section className="flex w-full max-w-6xl flex-col border-b-2 border-r-gray-500 lg:flex-row">
+        <div className="h-30 mt-6 w-full border-r-2 border-r-gray-100 lg:w-2/3 lg:pr-6">
+          <BigArticle title={title} content={content}></BigArticle>
         </div>
-        <div className="w-full lg:w-1/3 h-30 lg:ml-6 overflow-hidden">
-          <MiddleArticle title="[단독] 신명품 라인업 강화한 신세계인터내셔날, ㄴㄴㅇㄴㅇㅇㄹㄴㅇㄹ ‘더 로우’ 독점 유통" content="[이코노미스트 이혜리 기자] 패션전문기업 신세계인터내셔날이 ‘MZ세대의 에르메스’로 불리는 ‘더 로우’(The Row)를 새로 론칭하며 신명품 브랜드 포트폴리오 다각화에 나선다. "></MiddleArticle>
-          <MiddleArticle title="[단독] 신명품 라인업 강화한 신세계인터내셔날, ‘더 로우’ 독점 유통" content="[이코노미스트 이혜리 기자] 패션전문기업 신세계인터내셔날이 ‘MZ세대의 에르메스’로 불리는 ‘더 로우’(The Row)를 새로 론칭하며 신명품 브랜드 포트폴리오 다각화에 나선다. "></MiddleArticle>
-          <MiddleArticle title="[단독] 신명품 라인업 강화한 신세계인터내셔날, ‘더 로우’ 독점 유통" content="[이코노미스트 이혜리 기자] 패션전문기업 신세계인터내셔날이 ‘MZ세대의 에르메스’로 불리는 ‘더 로우’(The Row)를 새로 론칭하며 신명품 브랜드 포트폴리오 다각화에 나선다. "></MiddleArticle>
+        <div className="h-30 w-full overflow-hidden lg:ml-6 lg:w-1/3">
+          <MiddleArticle title={title} content={content}></MiddleArticle>
+          <MiddleArticle title={title} content={content}></MiddleArticle>
+          <MiddleArticle title={title} content={content}></MiddleArticle>
         </div>
       </section>
-      <section className="flex flex-col w-full lg:flex-row max-w-6xl border-b-2 border-r-gray-500">
-        <div className="w-full md:w-1/2 h-30 lg:w-1/3 lg:pr-6 border-r-2 border-r-gray-100">
-          <SmallArticle title="[단독] 신명품 라인업 강화한 신세계인터내셔날, ‘더 로우’ 독점 유통" content="[이코노미스트 이혜리 기자] 패션전문기업 신세계인터내셔날이 ‘MZ세대의 에르메스’로 불리는 ‘더 로우’(The Row)를 새로 론칭하며 신명품 브랜드 포트폴리오 다각화에 나선다. "></SmallArticle>
-          <SmallArticle title="[단독] 신명품 라인업 강화한 신세계인터내셔날, ‘더 로우’ 독점 유통" content="[이코노미스트 이혜리 기자] 패션전문기업 신세계인터내셔날이 ‘MZ세대의 에르메스’로 불리는 ‘더 로우’(The Row)를 새로 론칭하며 신명품 브랜드 포트폴리오 다각화에 나선다. "></SmallArticle>
+      <section className="flex w-full max-w-6xl flex-col border-b-2 border-r-gray-500 lg:flex-row">
+        <div className="h-30 w-full border-r-2 border-r-gray-100 md:w-1/2 lg:w-1/3 lg:pr-6">
+          <SmallArticle title={title} content={content}></SmallArticle>
+          <SmallArticle title={title} content={content}></SmallArticle>
         </div>
-        <div className="w-full md:w-1/2 h-30 lg:w-1/3 lg:px-6 border-r-2 border-r-gray-100">
-          <SmallArticle title="[단독] 신명품 라인업 강화한 신세계인터내셔날, ‘더 로우’ 독점 유통" content="[이코노미스트 이혜리 기자] 패션전문기업 신세계인터내셔날이 ‘MZ세대의 에르메스’로 불리는 ‘더 로우’(The Row)를 새로 론칭하며 신명품 브랜드 포트폴리오 다각화에 나선다. "></SmallArticle>
-          <SmallArticle title="[단독] 신명품 라인업 강화한 신세계인터내셔날, ‘더 로우’ 독점 유통" content="[이코노미스트 이혜리 기자] 패션전문기업 신세계인터내셔날이 ‘MZ세대의 에르메스’로 불리는 ‘더 로우’(The Row)를 새로 론칭하며 신명품 브랜드 포트폴리오 다각화에 나선다. "></SmallArticle>
-         </div>
-        <div className="w-full md:w-1/2 h-30 lg:w-1/3 lg:pl-6">
+        <div className="h-30 w-full border-r-2 border-r-gray-100 md:w-1/2 lg:w-1/3 lg:px-6">
+          <SmallArticle title={title} content={content}></SmallArticle>
+          <SmallArticle title={title} content={content}></SmallArticle>
+        </div>
+        <div className="h-30 w-full md:w-1/2 lg:w-1/3 lg:pl-6">
           <BigBanner></BigBanner>
         </div>
       </section>
-      <section className="flex flex-col w-full lg:flex-row max-w-6xl border-b-2 border-r-gray-500">
-        <div className="w-full md:w-1/2 h-30 lg:w-1/3 lg:pr-6 ">
+      <section className="flex w-full max-w-6xl flex-col border-b-2 border-r-gray-500 lg:flex-row">
+        <div className="h-30 w-full md:w-1/2 lg:w-1/3 lg:pr-6 ">
           <SmallBanner></SmallBanner>
         </div>
-        <div className="w-full md:w-1/2 h-30 lg:w-1/3 lg:px-6">
+        <div className="h-30 w-full md:w-1/2 lg:w-1/3 lg:px-6">
           <SmallBanner></SmallBanner>
         </div>
-        <div className="w-full md:w-1/2 h-30 lg:w-1/3 lg:pl-6">
+        <div className="h-30 w-full md:w-1/2 lg:w-1/3 lg:pl-6">
           <SmallBanner></SmallBanner>
         </div>
       </section>
-      <section className="flex flex-col w-full lg:flex-row max-w-6xl border-b-2 border-r-gray-500">
-        <div className="w-full md:w-1/2 h-30 lg:pr-6">
-          <MiddleArticle title="[단독] 신명품 라인업 강화한 신세계인터내셔날, ‘더 로우’ 독점 유통" content="[이코노미스트 이혜리 기자] 패션전문기업 신세계인터내셔날이 ‘MZ세대의 에르메스’로 불리는 ‘더 로우’(The Row)를 새로 론칭하며 신명품 브랜드 포트폴리오 다각화에 나선다. "></MiddleArticle>
-          <MiddleArticle title="[단독] 신명품 라인업 강화한 신세계인터내셔날, ‘더 로우’ 독점 유통" content="[이코노미스트 이혜리 기자] 패션전문기업 신세계인터내셔날이 ‘MZ세대의 에르메스’로 불리는 ‘더 로우’(The Row)를 새로 론칭하며 신명품 브랜드 포트폴리오 다각화에 나선다. "></MiddleArticle>
+      <section className="flex w-full max-w-6xl flex-col border-b-2 border-r-gray-500 lg:flex-row">
+        <div className="h-30 w-full md:w-1/2 lg:pr-6">
+          <MiddleArticle title={title} content={content}></MiddleArticle>
+          <MiddleArticle title={title} content={content}></MiddleArticle>
         </div>
-        <div className="w-full md:w-1/2 h-30 lg:pl-6">
-          <MiddleArticle title="[단독] 신명품 라인업 강화한 신세계인터내셔날, ‘더 로우’ 독점 유통" content="[이코노미스트 이혜리 기자] 패션전문기업 신세계인터내셔날이 ‘MZ세대의 에르메스’로 불리는 ‘더 로우’(The Row)를 새로 론칭하며 신명품 브랜드 포트폴리오 다각화에 나선다. "></MiddleArticle>
-          <MiddleArticle title="[단독] 신명품 라인업 강화한 신세계인터내셔날, ‘더 로우’ 독점 유통" content="[이코노미스트 이혜리 기자] 패션전문기업 신세계인터내셔날이 ‘MZ세대의 에르메스’로 불리는 ‘더 로우’(The Row)를 새로 론칭하며 신명품 브랜드 포트폴리오 다각화에 나선다. "></MiddleArticle>
+        <div className="h-30 w-full md:w-1/2 lg:pl-6">
+          <MiddleArticle title={title} content={content}></MiddleArticle>
+          <MiddleArticle title={title} content={content}></MiddleArticle>
         </div>
       </section>
-      <section className="flex flex-col w-full lg:flex-row max-w-6xl border-b-2 border-r-gray-500">
-        <div className="w-full md:w-1/2 h-30 lg:pr-6">
-          <MiddleArticle title="[단독] 신명품 라인업 강화한 신세계인터내셔날, ‘더 로우’ 독점 유통" content="[이코노미스트 이혜리 기자] 패션전문기업 신세계인터내셔날이 ‘MZ세대의 에르메스’로 불리는 ‘더 로우’(The Row)를 새로 론칭하며 신명품 브랜드 포트폴리오 다각화에 나선다. "></MiddleArticle>
-          <MiddleArticle title="[단독] 신명품 라인업 강화한 신세계인터내셔날, ‘더 로우’ 독점 유통" content="[이코노미스트 이혜리 기자] 패션전문기업 신세계인터내셔날이 ‘MZ세대의 에르메스’로 불리는 ‘더 로우’(The Row)를 새로 론칭하며 신명품 브랜드 포트폴리오 다각화에 나선다. "></MiddleArticle>
+      <section className="flex w-full max-w-6xl flex-col border-b-2 border-r-gray-500 lg:flex-row">
+        <div className="h-30 w-full md:w-1/2 lg:pr-6">
+          <MiddleArticle title={title} content={content}></MiddleArticle>
+          <MiddleArticle title={title} content={content}></MiddleArticle>
         </div>
-        <div className="w-full md:w-1/2 h-30 lg:pl-6">
-          <MiddleArticle title="[단독] 신명품 라인업 강화한 신세계인터내셔날, ‘더 로우’ 독점 유통" content="[이코노미스트 이혜리 기자] 패션전문기업 신세계인터내셔날이 ‘MZ세대의 에르메스’로 불리는 ‘더 로우’(The Row)를 새로 론칭하며 신명품 브랜드 포트폴리오 다각화에 나선다. "></MiddleArticle>
-          <MiddleArticle title="[단독] 신명품 라인업 강화한 신세계인터내셔날, ‘더 로우’ 독점 유통" content="[이코노미스트 이혜리 기자] 패션전문기업 신세계인터내셔날이 ‘MZ세대의 에르메스’로 불리는 ‘더 로우’(The Row)를 새로 론칭하며 신명품 브랜드 포트폴리오 다각화에 나선다. "></MiddleArticle>
+        <div className="h-30 w-full md:w-1/2 lg:pl-6">
+          <MiddleArticle title={title} content={content}></MiddleArticle>
+          <MiddleArticle title={title} content={content}></MiddleArticle>
         </div>
       </section>
       <Footer></Footer>
