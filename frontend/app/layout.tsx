63b721f2958ec8a4ae39449a13b4603150eda8ce
Jimin Ha <jimin9038@g.skku.edu>
2024-03-07 11:37:24 +0900

    format

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,22 +1,23 @@
-import type { Metadata } from "next";
-import { Noto_Sans_KR } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Noto_Sans_KR } from 'next/font/google'
+import './globals.css'
 
-const inter = Noto_Sans_KR({subsets:[]});
+const inter = Noto_Sans_KR({ subsets: [] })
 
 export const metadata: Metadata = {
-  title: "CEO TIME - The Magazine for Leaders",
-  description: "CEO TIME is a magazine for leaders. We cover the latest news, trends, and insights from the world of business and leadership.",
-};
+  title: 'CEO TIME - The Magazine for Leaders',
+  description:
+    'CEO TIME is a magazine for leaders. We cover the latest news, trends, and insights from the world of business and leadership.'
+}
 
 export default function RootLayout({
-  children,
+  children
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
